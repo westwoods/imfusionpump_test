15b45e635e5d6623b653ae188f94a948a42baeac
westwoods <kdw8681@gmail.com>
2017-09-20 05:39:04 +0000

    123

--- GPIO_sw_KeyPad.py
+++ GPIO_sw_KeyPad.py
@@ -18,8 +18,21 @@ from pygame.locals import *
 import RPi.GPIO as GPIO
 import time
 import ST7032I2C
-import pad4pi
+from pad4pi import rpi_gpio
 
+KEYPAD = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    ["*",0,"#"],
+    ]
+ROW_PINS = [5,6,13,19] # 2,7,6,4 
+COL_PINS = [26,20,21] #3,1,5
+factory = rpi_gpio.KeypadFactory()
+keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS,col_pins=COL_PINS)
+def printKey(key):
+    print(key)
+keypad.registerKeyPressHandler(printKey)
 def toggle_fullscreen():
     screen = pygame.display.get_surface()
     tmp = screen.convert()
